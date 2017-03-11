af779a11dd0c081e906c2cad16db4c6e713950d2
Michael Dean <michael.dean.1@outlook.com>
2017-03-11 16:34:51 +0000

    Adding padding and offer
    Adding padding to images to prevent overlap, added offer JS

--- Javascript/java.coffee
+++ Javascript/java.coffee
@@ -10,4 +10,24 @@ $(document).ready ->
 	year = year.getYear()
 	year = year - 112
 	$(".howlong").html year
-	#charcount()		
+	#charcount()
+	offer = ->
+		year = new Date()
+		todayDay = year.getDate()
+		todayMonth = year.getMonth()
+		todayYear = year.getYear() + 1900
+		offerperc = 0
+		#Christmas Offer
+		if todayMonth == 11 and todayDay >=20 or todayMonth == 12 and todayDay <=31 then offerperc = 25
+		#Easter Offer
+		a = todayYear % 19
+		b = ((11 * a) + 5) / 30
+		if b == 0 or (b==1 and a > 10) then c = b + 1 else c = b
+		if c >= 1 and c<=19 then M = 4 else M = 3
+		d = Math.floor((50 - c) % 31) - 1	#Easter falls on the Sunday after this date unless the date is a Sunday		
+		if todayMonth == M and todayDay >= d or todayMonth == (M + 1) and todayDay <=30 then offerperc = 25
+		if offerperc == 0 then offerMessage = "There isn't an offer on at the moment. But there will be one in the future." else offerMessage = "There is an offer on at the moment, claim now and you will get " + offerperc + "% off your order."
+		$(".offer").html offerMessage
+	offer()
+
+	
--- Javascript/java.js
+++ Javascript/java.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var charcount, year;
+  var charcount, offer, year;
   charcount = function() {
     $("p:not(:has(*))").each(function() {
       var str;
@@ -13,5 +13,39 @@ $(document).ready(function() {
   year = new Date();
   year = year.getYear();
   year = year - 112;
-  return $(".howlong").html(year);
+  $(".howlong").html(year);
+  offer = function() {
+    var M, a, b, c, d, offerMessage, offerperc, todayDay, todayMonth, todayYear;
+    year = new Date();
+    todayDay = year.getDate();
+    todayMonth = year.getMonth();
+    todayYear = year.getYear() + 1900;
+    offerperc = 0;
+    if (todayMonth === 11 && todayDay >= 20 || todayMonth === 12 && todayDay <= 31) {
+      offerperc = 25;
+    }
+    a = todayYear % 19;
+    b = ((11 * a) + 5) / 30;
+    if (b === 0 || (b === 1 && a > 10)) {
+      c = b + 1;
+    } else {
+      c = b;
+    }
+    if (c >= 1 && c <= 19) {
+      M = 4;
+    } else {
+      M = 3;
+    }
+    d = Math.floor((50 - c) % 31) - 1;
+    if (todayMonth === M && todayDay >= d || todayMonth === (M + 1) && todayDay <= 30) {
+      offerperc = 25;
+    }
+    if (offerperc === 0) {
+      offerMessage = "There isn't an offer on at the moment. But there will be one in the future.";
+    } else {
+      offerMessage = "There is an offer on at the moment, claim now and you will get " + offerperc + "% off your order.";
+    }
+    return $(".offer").html(offerMessage);
+  };
+  return offer();
 });
